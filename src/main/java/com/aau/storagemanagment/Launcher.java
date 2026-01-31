e71c81d46f0d34e97a9fb090fc2352547500f7f3
thomasugr-3173-17-crypto <thomas.ugr-3173-17@aau.edu.et>
2026-01-31 19:26:17 +0300

    Add files via upload

---
+++ src/main/java/com/aau/storagemanagment/Launcher.java
@@ -0,0 +1,9 @@
+package com.aau.storagemanagment;
+
+import com.aau.storagemanagment.MainApp;
+
+public class Launcher {
+    public static void main(String[] args) {
+        MainApp.main(args);
+    }
+}
