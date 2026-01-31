c24f88b5d007ed4c1512f16fe85e46afe6e2f1b2
natiman52 <natnaelyazchew135210@gmail.com>
2026-01-31 19:35:26 +0300

    final push

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.aau.storagemanagment {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
+    requires java.desktop;
 
     opens com.aau.storagemanagment to javafx.fxml;
     exports com.aau.storagemanagment;
